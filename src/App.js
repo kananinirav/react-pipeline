3ecbd3add6c5043ec25940b44de137d6944c862a
kananinirav <30398499+kananinirav@users.noreply.github.com>
2022-05-15 22:55:25 +0900

    Update App.js
    display msg changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>TESTING BITBUCKET PIPELINES WITH REACT</h1>
+        <h1>TESTING GITHUB ACTIONS AND BITBUCKET PIPELINES WITH REACT</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
