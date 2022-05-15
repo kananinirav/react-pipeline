0d63463b415f1ec089b05176211951e76a634c56
kananinirav <30398499+kananinirav@users.noreply.github.com>
2022-05-15 23:21:21 +0900

    Update App.js
    text updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>TESTING GITHUB ACTIONS AND BITBUCKET PIPELINES WITH REACT</h1>
+        <h1>Testing Github Actions And Bitbucket Pipelines With React</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
